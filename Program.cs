7162dc57b5fd9373ace8e76ae4d5725d187bdebc
parzival-au20 <74263381+parzival-au20@users.noreply.github.com>
2023-12-10 23:35:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -94,8 +94,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment() || app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 app.UseHttpsRedirection();
